cc789a8cd3056abe95b3ca2eb203e1fa3bcee218
aslamkhan1221 <aslamkhan1221@gmail.com>
2024-06-19 12:22:11 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,13 @@
+let age=15;
+let hasvoterid='no';
+
+if(age>=14){
+    if(hasvoterid == 'yes'){
+    alert("you can vote!");
+}
+else{
+    alert("Get your voter id card.");
+}
+}else{
+    alert("you can not vote");
+}
